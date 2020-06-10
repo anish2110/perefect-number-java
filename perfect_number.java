4a0c54adfc27cc3a0b10e890532a45a251d7d642
Anish Khandelwal <anishkhandelwal21@gmail.com>
2020-06-10 14:22:42 +0530

    Perfect Number Program

---
+++ perfect_number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Perfect{
+	public static void main(String arg[]){
+	    long n, sum = 0;
+        Scanner scan = new Scanner(System.in);
+        
+	    System.out.print("\nEnter a number: ");
+        n = scan.nextLong();
+        
+	    int i=1;
+	    while(i<=n/2){
+	       if(n%i==0){
+		      sum += i;
+	       }
+	      i++;
+	    }
+        
+	if(sum == n){
+	   System.out.println(n+" is a perfect number\n");
+    } 
+	else
+	   System.out.println(n+" is not a perfect number\n"); 
+	}
+}
